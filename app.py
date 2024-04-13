9d31f858a1a69fe92bdecdbfeede04912c344c2b
SaucyB03 <105938116+SaucyB03@users.noreply.github.com>
2024-04-13 16:49:47 -0400

    changes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    routes=[]
     time = datetime.now()
     day = datetime.today()
     today = day.isoweekday()
@@ -29,7 +30,6 @@ def home():
     df1 = df1[df1["Arrival at Next Stop"] > str(time)]
 
 
-    routes =[]
     data = {}
     for col in df1.columns:
         data[col]=df1[col].iloc[0]
@@ -38,80 +38,65 @@ def home():
 
 
 
-    # if today < 5:
-    #     df2 = df2[df2["Days"] == "Weekdays"]
-    # elif today == 5:
-    #     df2 = df2[df2["Days"] == "Saturday"]
-    # elif today == 6:
-    #     df2 = df2[df2["Days"] == "Sunday"]
+    if today < 5:
+        df2 = df2[df2["Days"] == "Weekdays"]
+    elif today == 5:
+        df2 = df2[df2["Days"] == "Saturday"]
+    elif today == 6:
+        df2 = df2[df2["Days"] == "Sunday"]
 
-    # df2 = df2.sort_values(by=["Closest Stop Arrival Time"])
-    # df2 = df2[df2["Closest Stop Arrival Time"] > str(time)]
+    df2['Closest Stop Arrival Time'] = df2['Closest Stop Arrival Time'].map(lambda x: pd.to_datetime(x))
+    df2['Arrival at Next Stop'] = df2['Arrival at Next Stop'].map(lambda x: pd.to_datetime(x))
 
-
-    # routes =[]
-    # data = {}
-    # for col in df2.columns:
-    #     data[col]=df2[col][0]
-    # data['line'] = 'City Route'
-    # routes.append(data)
+    df2 = df2.sort_values(by=["Closest Stop Arrival Time"])
+    df2 = df2[df2["Arrival at Next Stop"] > time]
 
 
-    # if today < 5:
-    #     df3 = df3[df3["Days"] == "Weekdays"]
-    # elif today == 5:
-    #     df3 = df3[df3["Days"] == "Saturday"]
-    # elif today == 6:
-    #     df3 = df3[df3["Days"] == "Sunday"]
+    data = {}
+    for col in df2.columns:
+        data[col]=df2[col].iloc[0]
+    data['line'] = 'City Route'
+    routes.append(data)
 
-    # df3 = df3.sort_values(by=["Closest Stop Arrival Time"])
-    # df3 = df3[df3["Closest Stop Arrival Time"] > str(time)]
+    if today < 5:
+        df3 = df3[df3["Days"] == "Weekdays"]
+    elif today == 5:
+        df3 = df3[df3["Days"] == "Saturday"]
+    elif today == 6:
+        df3 = df3[df3["Days"] == "Sunday"]
 
+    df3['Closest Stop Arrival Time'] = df3['Closest Stop Arrival Time'].map(lambda x: pd.to_datetime(x))
+    df3['Arrival at Next Stop'] = df3['Arrival at Next Stop'].map(lambda x: pd.to_datetime(x))
 
-    # routes =[]
-    # data = {}
-    # for col in df3.columns:
-    #     data[col]=df3[col][0]
-    # data['line'] = 'Essex Route'
-    # routes.append(data)
-
-    # if today < 5:
-    #     df4 = df4[df4["Days"] == "Weekdays"]
-    # elif today == 5:
-    #     df4 = df4[df4["Days"] == "Saturday"]
-    # elif today == 6:
-    #     df4 = df4[df4["Days"] == "Sunday"]
-
-    # df4 = df4.sort_values(by=["Closest Stop Arrival Time"])
-    # df4 = df4[df4["Closest Stop Arrival Time"] > str(time)]
+    df3 = df3.sort_values(by=["Closest Stop Arrival Time"])
+    df3 = df3[df3["Arrival at Next Stop"] > str(time)]
 
 
-    # routes =[]
-    # data = {}
-    # for col in df4.columns:
-    #     data[col]=df4[col][0]
-    # data['line'] = 'Williston Route'
-    # routes.append(data)
-    
+    data = {}
+    for col in df3.columns:
+        data[col]=df3[col].iloc[0]
+    data['line'] = 'Essex Route'
+    routes.append(data)
 
-    # for arrival in data['Closest Stop Arrival Time']:
-    #     if arrival > time:
-    #         data['Closest Stop Arrival Time'] = arrival
+    if today < 5:
+        df4 = df4[df4["Days"] == "Weekdays"]
+    elif today == 5:
+        df4 = df4[df4["Days"] == "Saturday"]
+    elif today == 6:
+        df4 = df4[df4["Days"] == "Sunday"]
 
-    # data = {}
-    # for col in df2.columns:
-    #     data[col]=list(df2[col])
-    # routes.append(data)
+    df4['Closest Stop Arrival Time'] = df4['Closest Stop Arrival Time'].map(lambda x: pd.to_datetime(x))
+    df4['Arrival at Next Stop'] = df4['Arrival at Next Stop'].map(lambda x: pd.to_datetime(x))
 
-    # data = {}
-    # for col in df3.columns:
-    #     data[col]=list(df3[col])
-    # routes.append(data)
+    df4 = df4.sort_values(by=["Closest Stop Arrival Time"])
+    df4 = df4[df4["Arrival at Next Stop"] > str(time)]
 
-    # for col in df4.columns:
-    #     data[col]=list(df4[col])
-    # routes.append(data)
-        
+    data = {}
+    for col in df4.columns:
+        data[col]=df4[col].iloc[0]
+    data['line'] = 'Williston Route'
+    routes.append(data)
+   
 
     return render_template('index.html', data=routes)
 
