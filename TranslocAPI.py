831298131b1cdafad0d1d23743d18b39739bb78a
jordanvtskier1 <jmgottli@uvm.edu>
2024-04-13 12:46:43 -0400

    Stops and routes dataframes

--- TranslocAPI.py
+++ TranslocAPI.py
@@ -7,6 +7,9 @@ import pandas as pd
 
 stopsurl = "https://transloc-api-1-2.p.rapidapi.com/stops.json"
 
+routesurl = "https://transloc-api-1-2.p.rapidapi.com/routes.json"
+
+
 querystring = {"agencies":"603","callback":"call"}
 
 headers = {
@@ -14,9 +17,34 @@ headers = {
 	"X-RapidAPI-Host": "transloc-api-1-2.p.rapidapi.com"
 }
 
-response = requests.get(stopsurl, headers=headers, params=querystring)
+responseStops = requests.get(stopsurl, headers=headers, params=querystring)
 
-print(response.json())
+dfStops = pd.DataFrame.from_dict(responseStops.json()['data'])
 
-df = pd.DataFrame.from_dict(response.json()['data'])
-print(df)
+columns_to_keep = ['location','stop_id','routes','name']
+columns_to_drop = set(dfStops.columns) - set(columns_to_keep)
+
+"""
+ Dataframe of all stops
+ Columns: [
+ location  -- format: {'lat': 44.467529, 'lng': -73.198875}
+ stop id   -- unique int
+ routes    -- list of routes on stop
+ name      -- name of stop
+ ]
+"""
+dfStops = dfStops.drop(columns=columns_to_drop)
+print(dfStops)
+
+
+responseRoutes = requests.get(routesurl, headers=headers, params=querystring)
+dfRoutes = dfStops = pd.DataFrame.from_dict(responseRoutes.json()['data']['603'])
+
+
+dfRoutes = dfRoutes.drop(0,axis=0)
+
+
+pd.set_option('display.max_colwidth', None)
+print(dfRoutes)
+
+
